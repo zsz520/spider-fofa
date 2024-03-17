a2b5bd9eec8d5eadd37a2fac92bd08050a976f9c
小朱同学 <43113023+zsz520@users.noreply.github.com>
2024-03-17 14:46:53 +0800

    Update All.py

--- All.py
+++ All.py
@@ -50,35 +50,38 @@ urls = {
 
     
 }
-
 # 遍历网页列表
 for url, filename in urls.items():
-    print(f'正在爬取{filename}.....')
-    # 发送GET请求获取源代码
-    response = requests.get(url)
-    page_content = response.text
-    # 查找所有符合指定格式的网址
-    pattern = r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+"
-    urls_all = re.findall(pattern, page_content)
-    urls = set(urls_all)  # 去重得到唯一的URL列表
-    existing_urls = []
-    # 检查文件是否存在，如果不存在则创建文件
-    if not os.path.exists(filename):
-        with open(filename, 'w', encoding='utf-8'):
-            pass
-    # 读取已存在的URL
-    with open(filename, 'r', encoding='utf-8') as file:
-        existing_urls = file.readlines()
-    existing_urls = [url.strip() for url in existing_urls]  # 去除每行末尾的换行符
-    with open(filename, 'r+', encoding='utf-8') as file:
-        content = file.read()
-        file.seek(0, 0)  # 将文件指针移到文件开头
-        for url in urls:
-            if url not in existing_urls:
-                file.write(url + "\n")
-                print(url)
-                existing_urls.append(url)  # 将新写入的URL添加到已存在的URL列表中
-        file.write(content)  # 将原有内容写回文件
+    try:
+        print(f'正在爬取{filename}.....')
+        # 发送GET请求获取源代码
+        response = requests.get(url)
+        page_content = response.text
+        # 查找所有符合指定格式的网址
+        pattern = r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+"
+        urls_all = re.findall(pattern, page_content)
+        urls = set(urls_all)  # 去重得到唯一的URL列表
+        existing_urls = []
+        # 检查文件是否存在，如果不存在则创建文件
+        if not os.path.exists(filename):
+            with open(filename, 'w', encoding='utf-8'):
+                pass
+        # 读取已存在的URL
+        with open(filename, 'r', encoding='utf-8') as file:
+            existing_urls = file.readlines()
+        existing_urls = [url.strip() for url in existing_urls]  # 去除每行末尾的换行符
+        with open(filename, 'r+', encoding='utf-8') as file:
+            content = file.read()
+            file.seek(0, 0)  # 将文件指针移到文件开头
+            for url in urls:
+                if url not in existing_urls:
+                    file.write(url + "\n")
+                    print(url)
+                    existing_urls.append(url)  # 将新写入的URL添加到已存在的URL列表中
+            file.write(content)  # 将原有内容写回文件
+    except Exception as e:
+        print(f"获取URL {url} 失败：{str(e)}")
+        continue
     # 暂停2秒
     time.sleep(2)
     print(f'{filename}爬取完毕,下一个')
