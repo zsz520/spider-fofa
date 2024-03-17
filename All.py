8f1507560fc485b5c4386fdf75a5ae1279f49842
小朱同学 <43113023+zsz520@users.noreply.github.com>
2024-03-17 14:32:28 +0800

    Update All.py

--- All.py
+++ All.py
@@ -54,22 +54,9 @@ urls = {
 # 遍历网页列表
 for url, filename in urls.items():
     print(f'正在爬取{filename}.....')
-        # 创建一个Chrome WebDriver实例
-    chrome_options = Options()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-
-    driver = webdriver.Chrome(options=chrome_options)
-    # 使用WebDriver访问网页
-    driver.get(url)  # 将网址替换为你要访问的网页地址
-    time.sleep(10)
-    # 获取网页内容
-    page_content = driver.page_source
-
-    # 关闭WebDriver
-    driver.quit()
-
+    # 发送GET请求获取源代码
+    response = requests.get(url)
+    page_content = response.text
     # 查找所有符合指定格式的网址
     pattern = r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+"
     urls_all = re.findall(pattern, page_content)
